d7b005cb9d4bee7c03a46b3a2723d8d982ab5a3f
CoelhoKillerBR <marlondesouzajlle@hotmail.com>
2023-09-15 21:52:32 -0300

    paginação basica add

---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+function Login (){
+    return (
+        <div>
+            <h1>
+                Login 
+            </h1>
+                <p>
+                <Link href = '/'>voltar para pag inicial</Link>
+                </p>
+        </div>
+    );
+}
+
+export default Login; 
