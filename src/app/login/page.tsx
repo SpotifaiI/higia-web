a939b02b5d24d1934139afb7321bf42ea7e45db0
Cristian Prochnow <cristianprochnow@gmail.com>
2023-11-02 20:38:52 -0300

    feat: add skeleton for form from login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,30 +2,93 @@ import {
   BrandContainer,
   BrandLogoContainer,
   BrandLogoImage,
-  Container, LoginContainer, LoginHeader, LoginIcon, LoginIconContainer, LoginTitle
+  Container,
+  FormFieldContainer,
+  FormFieldLabel,
+  GradientActionButton,
+  IconActionButton,
+  IconActionButtonImage,
+  IconActionButtonImageContainer,
+  LoginContainer,
+  LoginContainerWrapper,
+  LoginFooter,
+  LoginForm,
+  LoginHeader,
+  LoginIcon,
+  LoginIconContainer,
+  LoginTitle,
+  Separator,
+  SeparatorContainer,
+  SeparatorText
 } from "./styles";
 
 function Login() {
   return (
     <Container>
       <LoginContainer>
-        <LoginHeader>
-          <LoginIconContainer>
-            <LoginIcon
-              src="/assets/images/higia-accent.png"
-              alt="Logo Higia"
-              fill={true} />
-          </LoginIconContainer>
-          <LoginTitle>
-            Faça login para começar!
-          </LoginTitle>
-        </LoginHeader>
+        <LoginContainerWrapper>
+          <LoginHeader>
+            <LoginIconContainer>
+              <LoginIcon
+                src="/assets/images/higia-accent.png"
+                alt="Logo Higia"
+                fill={true} />
+            </LoginIconContainer>
+            <LoginTitle>
+              Faça login para começar!
+            </LoginTitle>
+          </LoginHeader>
+
+          <LoginForm>
+            <FormFieldContainer>
+              <FormFieldLabel>Label</FormFieldLabel>
+              <FormFieldLabel />
+            </FormFieldContainer>
+
+            <FormFieldContainer>
+              <FormFieldLabel>Label</FormFieldLabel>
+              <FormFieldLabel />
+            </FormFieldContainer>
+
+            <GradientActionButton type="button" />
+          </LoginForm>
+
+          <SeparatorContainer>
+            <Separator />
+            <SeparatorText>ou</SeparatorText>
+            <Separator />
+          </SeparatorContainer>
+
+          <LoginFooter>
+            <IconActionButton type="button">
+              <IconActionButtonImageContainer>
+                <IconActionButtonImage
+                  src="/assets/images/google.png"
+                  alt="Google Logo"
+                  fill={true} />
+
+                Entrar com Google
+              </IconActionButtonImageContainer>
+            </IconActionButton>
+
+            <IconActionButton type="button">
+              <IconActionButtonImageContainer>
+                <IconActionButtonImage
+                  src="/assets/images/google.png"
+                  alt="Google Logo"
+                  fill={true} />
+
+                Entrar com Google
+              </IconActionButtonImageContainer>
+            </IconActionButton>
+          </LoginFooter>
+        </LoginContainerWrapper>
       </LoginContainer>
 
       <BrandContainer>
         <BrandLogoContainer>
           <BrandLogoImage
-            src="/assets/images/higia-accent.png"
+            src="/assets/images/higia-accent-dark.png"
             alt="Logo Higia"
             fill={true} />
         </BrandLogoContainer>
--- src/app/login/styles.ts
+++ src/app/login/styles.ts
@@ -14,23 +14,86 @@ export const LoginContainer = styled.main`
   min-height: 100vh;
 `;
 
+export const LoginContainerWrapper = styled.div`
+  width: 80%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+`;
+
 export const BrandContainer = styled.aside`
   min-height: 100vh;
   background: linear-gradient(to right bottom, #37FC6E, #5A81FA);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const BrandLogoContainer = styled.div`
   position: relative;
+  height: 10rem;
+  width: 50%;
 `;
 
-export const BrandLogoImage = styled(Image)``;
+export const BrandLogoImage = styled(Image)`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
 
-export const LoginHeader = styled.header``;
+export const LoginHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export const LoginIconContainer = styled.div`
   position: relative;
+  height: 4.8rem;
+  width: 4.8rem;
 `;
 
-export const LoginIcon = styled(Image)``;
+export const LoginIcon = styled(Image)`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
 
-export const LoginTitle = styled.p``;
+export const LoginTitle = styled.p`
+  font-size: 1.6rem;
+  line-height: 2rem;
+  color: var(--color-darker);
+  font-weight: 700;
+  text-align: center;
+`;
+
+export const LoginForm = styled.form``;
+
+export const FormFieldContainer = styled.div``;
+
+export const FormFieldLabel = styled.label``;
+
+export const FormField = styled.input``;
+
+export const GradientActionButton = styled.button``;
+
+export const SeparatorContainer = styled.div``;
+
+export const Separator = styled.span``;
+
+export const SeparatorText = styled.p``;
+
+export const IconActionButton = styled.button``;
+
+export const IconActionButtonImageContainer = styled.div`
+  position: relative;
+`;
+
+export const IconActionButtonImage = styled(Image)`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export const LoginFooter = styled.footer``;
