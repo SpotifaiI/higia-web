2e67269bc6056cfe357895882f2cce277f7c4c53
Lucaswillians <lucaswilianserpa@gmail.com>
2023-11-14 20:38:05 -0300

    feat: add taskMap

--- src/app/tasks-map/page.tsx
+++ src/app/tasks-map/page.tsx
@@ -1,12 +1,61 @@
-import { AppWrapper } from "@/components/AppWrapper";
-import {} from "./styles";
+'use client';
 
-function TasksMap() {
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { CheckCircle, Clock, Zap, PlusCircle } from 'react-feather';
+import { AppWrapper } from '@/components/AppWrapper';
+import { FormFieldInput } from '@/components/FormFieldInput';
+import { TaskList } from '@/components/TaskList';
+import { GradientActionButton } from '@/components/GradientActionButton';
+import {
+  MapLayout,
+  SearchContainer,
+  SearchFieldsGroup,
+} from './styles';
+import { colors } from "@/global/theme";
+
+function TaskMap() {
+  const position = {
+    lat: 51.505,
+    lng: -0.09
+  }; // coordenadas iniciais
+
   return (
-    <AppWrapper title="Mapa de Tarefas">
-      <p>Aqui está o mapa de tarefas</p>
+    <AppWrapper title="Tarefas">
+      <SearchContainer>
+        <SearchFieldsGroup>
+          <FormFieldInput label="Data Início" />
+          <FormFieldInput label="Data Fim" />
+        </SearchFieldsGroup>
+
+        <GradientActionButton>
+          Buscar
+        </GradientActionButton>
+      </SearchContainer>
+
+      {/* Mapa React Leaflet */}
+      <MapLayout>
+        <MapContainer 
+          center={position} 
+          zoom={13} 
+          scrollWheelZoom={false}
+          style={{
+            width: '100%',
+            height: 700
+          }}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </MapLayout>
     </AppWrapper>
   );
 }
 
-export default TasksMap;
+export default TaskMap;
--- src/app/tasks-map/styles.ts
+++ src/app/tasks-map/styles.ts
@@ -1,1 +1,49 @@
+'use client';
+
 import styled from "styled-components";
+import { sizes } from '@/global/sizes';
+import { colors } from '@/global/theme';
+
+export const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: .8rem;
+  background: ${colors.light};
+  padding: 1.6rem;
+  border-radius: 1.6rem;
+
+    @media (min-width: ${sizes.laptop}) {
+      flex-direction: row;
+
+    & > button {
+      align-self: flex-end;
+      height: 4rem;
+    }
+  }
+`;
+
+
+export const SearchFieldsGroup = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: .8rem;
+
+  @media (min-width: ${sizes.tablet}) {
+    flex-direction: row;
+
+    & > * {
+      flex: 1;
+    }
+  }
+
+    @media (min-width: ${sizes.laptop}) {
+      width: 100%;
+    }
+
+`;
+
+export const MapLayout = styled.main`
+  height: 100%;
+  width: 100vh;
+  position: relative;
+`;
