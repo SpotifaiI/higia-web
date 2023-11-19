103c8034141a6a5e585033ca2200313e25b17b70
gustapine <gustavohd34@gmail.com>
2023-11-19 17:57:59 -0300

    feat: profile page

---
+++ src/components/ProfileDetails/index.tsx
@@ -0,0 +1,19 @@
+import {
+  ProfileDetailsContainer,
+  ProfileDetailsData,
+  ProfileDetailsLabel,
+} from "./styles";
+
+export type ProfileDetailsProps = {
+  label: string;
+  text: string;
+};
+
+export function ProfileDetails({ label, text }: ProfileDetailsProps) {
+  return (
+    <ProfileDetailsContainer>
+      <ProfileDetailsLabel>{label}</ProfileDetailsLabel>
+      <ProfileDetailsData>{text}</ProfileDetailsData>
+    </ProfileDetailsContainer>
+  );
+}
