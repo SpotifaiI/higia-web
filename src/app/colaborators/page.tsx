47b8352e696ee55b399b2f15587198300036c4e7
Marlon-Souza16 <marlondesouzajlle@hotmail.com>
2023-11-22 21:24:46 -0300

    Colaborators page started

--- src/app/colaborators/page.tsx
+++ src/app/colaborators/page.tsx
@@ -1,10 +1,50 @@
 import { AppWrapper } from "@/components/AppWrapper";
-import {} from "./styles";
+import { Users, PauseCircle } from "react-feather";
+import { TaskList } from '@/components/TaskList';
+import { ColaboratorsEmail, ColaboratorsMainFont, ColaboratorsIncialDate, ColaboratorsName, ColaboratorsTitle, ColaboratorsFont } from "./styles";
 
 function Colaborators() {
   return (
     <AppWrapper title="Colaboradores">
-      <p>Aqui estão os colaboradores</p>
+      <ColaboratorsTitle>
+     
+        <ColaboratorsName>
+        
+          <ColaboratorsMainFont>NOME</ColaboratorsMainFont>
+
+          <ColaboratorsFont>Lucas Cristian de Souza</ColaboratorsFont>
+          <ColaboratorsFont>Marlon de Souza</ColaboratorsFont>
+
+        
+        </ColaboratorsName>
+
+      
+      
+        <ColaboratorsEmail>
+        
+          <ColaboratorsMainFont>E-MAIL</ColaboratorsMainFont>
+
+          <ColaboratorsFont>lucas@hotmail.com</ColaboratorsFont>
+          <ColaboratorsFont>marlon@hotmail.com</ColaboratorsFont>
+
+
+        
+        </ColaboratorsEmail>
+
+        
+        <ColaboratorsIncialDate>
+        
+         <ColaboratorsMainFont>CRIAÇÃO</ColaboratorsMainFont>
+
+         <ColaboratorsFont>02/09/2001</ColaboratorsFont>
+         <ColaboratorsFont>16/09/2003</ColaboratorsFont>
+
+
+
+        </ColaboratorsIncialDate>
+      
+      
+      </ColaboratorsTitle>
     </AppWrapper>
   );
 }
--- src/app/colaborators/styles.ts
+++ src/app/colaborators/styles.ts
@@ -1,1 +1,31 @@
+'use client';
 import styled from "styled-components";
+
+
+export const ColaboratorsTitle = styled.div`
+display: grid;
+grid-template-columns: 1fr;
+justify-content: space-around;
+align-items: center;
+width: 100%;
+`;
+
+export const ColaboratorsInfo = styled.div`
+display: flex;
+flex-direction: column;
+width: 100%;
+margin-top: .8rem;
+`;
+
+export const ColaboratorsName = styled(ColaboratorsInfo)``;
+export const ColaboratorsEmail = styled(ColaboratorsInfo)``;
+export const ColaboratorsIncialDate = styled(ColaboratorsInfo)``;
+
+export const ColaboratorsMainFont = styled.p`
+font-size: 1.5rem;
+`;
+
+export const ColaboratorsFont = styled.p`
+font-size: 1rem;
+margin-top: .5rem;
+`;
