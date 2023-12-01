358e3095391b7d73a14ecf64109b8992e7ef0cfa
Cristian Prochnow <cristianprochnow@gmail.com>
2023-12-01 01:33:37 -0300

    fix: fix conflicts

--- src/app/tasks-map/page.tsx
+++ src/app/tasks-map/page.tsx
@@ -1,19 +1,12 @@
 'use client';
 
-<<<<<<< HEAD
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from 'react';
-import React from 'react';
 import { TasksAPI } from '@/api/tasks/tasks';
 import { Task } from "@/api/tasks/tasks.model";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { CheckCircle, Clock, Zap, PlusCircle } from 'react-feather';
-=======
->>>>>>> c408351be91891bb52bb7476b3e9f3bf89be162e
 import { AppWrapper } from '@/components/AppWrapper';
 import { FormFieldInput } from '@/components/FormFieldInput';
 import { GradientActionButton } from '@/components/GradientActionButton';
 import { TaskMap as TaskMapComponent } from '@/components/TaskMap';
+import { useEffect, useState } from 'react';
 import {
   SearchContainer,
   SearchFieldsGroup,
