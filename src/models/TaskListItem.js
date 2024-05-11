1669e990ed15fa27950d2069c5286a2399056af0
Cristian Prochnow <cristianprochnow@gmail.com>
2024-05-11 01:00:40 -0300

    feat: standardize table layout from task screen

--- src/models/TaskListItem.js
+++ src/models/TaskListItem.js
@@ -1,26 +1,77 @@
-export class TaskListItem {
-  /**
-   * @type {string}
-   */
-  title;
-  /**
-   * @type {string}
-   */
-  person;
-  /**
-   * @type {number}
-   */
-  distanceInKm;
+export const LOW_PRIORITY = 1;
+export const MEDIUM_PRIORITY = 2;
+export const HIGH_PRIORITY = 3;
+export const PENDING_STATUS = 1;
+export const ACTIVE_STATUS = 2;
+export const FINISHED_STATUS = 3;
 
-  /**
-   * @param {string} title
-   * @param {string} person
-   * @param {number} distanceInKm
-   * @private
-   */
-  constructor(title, person, distanceInKm) {
+export class TaskListItem {
+  id;
+  title;
+  person;
+  distanceInKm;
+  priority;
+  status;
+
+  constructor(
+    id,
+    title,
+    people,
+    distanceInKm,
+    priority = LOW_PRIORITY,
+    status = PENDING_STATUS
+  ) {
+    this.id = id;
     this.title = title;
-    this.person = person;
+    this.person = this.getPeople(people);
     this.distanceInKm = distanceInKm;
+    this.priority = this.getPriority(priority);
+    this.status = this.getStatus(status);
+  }
+
+  getPeople(people) {
+    let peopleFormat = [];
+
+    if (typeof people === 'string') {
+      peopleFormat = [people];
+    }
+
+    return peopleFormat;
+  }
+
+  getPriorityMap() {
+    return {
+      [LOW_PRIORITY]: 'low',
+      [MEDIUM_PRIORITY]: 'medium',
+      [HIGH_PRIORITY]: 'high'
+    };
+  }
+
+  getStatusMap() {
+    return {
+      [PENDING_STATUS]: 'pending',
+      [ACTIVE_STATUS]: 'active',
+      [FINISHED_STATUS]: 'finished'
+    };
+  }
+
+  getPriority(priority) {
+    const priorityMeaning = this.getPriorityMap()[priority];
+
+    if (!priorityMeaning) {
+      return this.getPriorityMap()[LOW_PRIORITY];
+    }
+
+    return priorityMeaning;
+  }
+
+  getStatus(status) {
+    const statusMeaning = this.getStatusMap()[status];
+
+    if (!statusMeaning) {
+      return this.getStatusMap()[PENDING_STATUS];
+    }
+
+    return statusMeaning;
   }
 }
--- src/screens/Tasks/index.jsx
+++ src/screens/Tasks/index.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CheckCircle, Clock, PlusCircle, Zap } from 'react-feather';
 
 import { colors } from '../../global/theme.js';
+import {
+  ACTIVE_STATUS, FINISHED_STATUS,
+  PENDING_STATUS,
+  TaskListItem,
+} from '../../models/TaskListItem.js';
 import { AppWrapper } from '../../components/AppWrapper/index.jsx';
 import { FormFieldInput } from '../../components/FormFieldInput/index.jsx';
 import { TaskList } from '../../components/TaskList/index.jsx';
@@ -14,30 +20,175 @@ import {
   TaskListGroup,
   TaskListTools,
   TaskTable,
+  TaskTableBody,
   TaskTableCell,
-  TaskTableHeader,
+  TaskTableContainer, TaskTableHead,
+  TaskTableHeader, TaskTableItem,
   TaskTableRow,
 } from './styles.js';
-import { TaskListItem } from '../../models/TaskListItem.js';
-import { useNavigate } from 'react-router-dom';
 
 export function Tasks() {
-  const [pendingTasks, setPendingTasks] = useState([]);
-  const [activeTasks, setActiveTasks] = useState([]);
-  const [concludedTasks, setConcludedTasks] = useState([]);
-
   const navigation = useNavigate();
 
+  const [tasks, setTasks] = useState({
+    [PENDING_STATUS]: [],
+    [ACTIVE_STATUS]: [],
+    [FINISHED_STATUS]: []
+  });
+
   useEffect(() => {
-    (async () => {
-      // const tasksApi = new TasksAPI();
-      // const { pending, active, concluded } = await tasksApi.getOrderedTasks();
-      //
-      // setPendingTasks(pending);
-      // setActiveTasks(active);
-      // setConcludedTasks(concluded);
-    })();
+    setTasks(loadTasks());
   }, []);
+
+  function loadTasks() {
+    const elementsTasksList = {
+      [PENDING_STATUS]: [],
+      [ACTIVE_STATUS]: [],
+      [FINISHED_STATUS]: []
+    };
+    const tasksList = sortTasks(fetchTasks());
+    const tasksLength = getTasksLength(tasksList);
+
+    let index;
+
+    for (index = 0; index < tasksLength; index++) {
+      if (tasksList[PENDING_STATUS][index]) {
+        elementsTasksList[PENDING_STATUS]
+          .push(tasksList[PENDING_STATUS][index]);
+      } else {
+        elementsTasksList[PENDING_STATUS]
+          .push({});
+      }
+      if (tasksList[ACTIVE_STATUS][index]) {
+        elementsTasksList[ACTIVE_STATUS]
+          .push(tasksList[ACTIVE_STATUS][index]);
+      } else {
+        elementsTasksList[ACTIVE_STATUS]
+        .push({});
+      }
+      if (tasksList[FINISHED_STATUS][index]) {
+        elementsTasksList[FINISHED_STATUS]
+          .push(tasksList[FINISHED_STATUS][index]);
+      } else {
+        elementsTasksList[FINISHED_STATUS]
+        .push({});
+      }
+    }
+
+    return elementsTasksList;
+  }
+
+  function sortTasks(tasksList) {
+    const sortTasks = {};
+
+    for (let taskItem of tasksList) {
+      if (!sortTasks[taskItem.status]) {
+        sortTasks[taskItem.status] = [];
+      }
+
+      sortTasks[taskItem.status].push(taskItem);
+    }
+
+    return sortTasks;
+  }
+
+  function getTasksLength(tasks) {
+    let greaterTasksAmount = 0;
+
+    [PENDING_STATUS, ACTIVE_STATUS, FINISHED_STATUS].map(status => {
+      if (tasks[status] && tasks[status].length > greaterTasksAmount) {
+        greaterTasksAmount = tasks[status].length;
+      }
+    });
+
+    return greaterTasksAmount;
+  }
+
+  function fetchTasks() {
+    return [
+      new TaskListItem(
+        1,
+        'Avenida JK',
+        [
+          'João', 'Pedro'
+        ],
+        1,
+        2
+      ),
+      new TaskListItem(
+        2,
+        'Clareira Amoenta',
+        [
+          'João', 'Pedro'
+        ],
+        1,
+        2
+      ),
+      new TaskListItem(
+        3,
+        'Alameda Ibituba',
+        [
+          'João', 'Pedro'
+        ],
+        1,
+        2
+      ),
+      new TaskListItem(
+        4,
+        'Aluguel Ausente',
+        [
+          'João', 'Pedro'
+        ],
+        1,
+        2
+      ),
+      new TaskListItem(
+        5,
+        'Compra Venda',
+        [
+          'João', 'Pedro'
+        ],
+        1,
+        2
+      ),
+      new TaskListItem(
+        6,
+        'Aluguel Anônimo',
+        [
+          'João', 'Pedro'
+        ],
+        1,
+        2
+      ),
+      new TaskListItem(
+        7,
+        'Avenida Da Saudade',
+        [
+          'João', 'Pedro'
+        ],
+        1,
+        2
+      ),
+      new TaskListItem(
+        8,
+        'Avenida XV',
+        [
+          'João', 'Pedro'
+        ],
+        1,
+        2
+      ),
+      new TaskListItem(
+        9,
+        'Rua da Alegria',
+        [
+          'João', 'Pedro'
+        ],
+        1,
+        2
+      )
+    ];
+  }
 
   function onAddTaskHandler() {
      navigation('/tasks/register');
@@ -67,23 +218,43 @@ export function Tasks() {
         </GradientActionButton>
       </TaskListTools>
 
-      <TaskTable>
-        <TaskTableHeader>
-          <TaskTableRow>
-            <TaskTableCell>
-              <Clock size={24} /> Pendentes
-            </TaskTableCell>
+      <TaskTableContainer>
+        <TaskTable>
+          <TaskTableHeader>
+            <TaskTableRow>
+              <TaskTableHead>
+                <TaskTableItem>
+                  <Clock size={24} /> Pendentes
+                </TaskTableItem>
+              </TaskTableHead>
 
-            <TaskTableCell>
-              <Zap size={24} /> Ativas
-            </TaskTableCell>
+              <TaskTableHead>
+                <TaskTableItem>
+                  <Zap size={24} /> Ativas
+                </TaskTableItem>
+              </TaskTableHead>
 
-            <TaskTableCell>
-              <CheckCircle size={24} /> Concluídas
-            </TaskTableCell>
-          </TaskTableRow>
-        </TaskTableHeader>
-      </TaskTable>
+              <TaskTableHead>
+                <TaskTableItem>
+                  <CheckCircle size={24} /> Concluídas
+                </TaskTableItem>
+              </TaskTableHead>
+            </TaskTableRow>
+          </TaskTableHeader>
+
+          <TaskTableBody>
+            {
+              tasks[PENDING_STATUS].map((_, index) => (
+                <TaskTableRow key={index}>
+                  <TaskTableCell>{tasks[PENDING_STATUS][index]}</TaskTableCell>
+                  <TaskTableCell>{tasks[ACTIVE_STATUS][index]}</TaskTableCell>
+                  <TaskTableCell>{tasks[FINISHED_STATUS][index]}</TaskTableCell>
+                </TaskTableRow>
+              ))
+            }
+          </TaskTableBody>
+        </TaskTable>
+      </TaskTableContainer>
 
       <TaskListGroup>
         <TaskList
