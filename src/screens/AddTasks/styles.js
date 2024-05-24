6702591a5ed7690aa647d7ccc985655cd91c9b0b
Cristian Prochnow <cristian.prochnow@ascensus.com.br>
2024-05-24 08:31:20 -0300

    feat: add inputs for task priority

--- src/screens/AddTasks/styles.js
+++ src/screens/AddTasks/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import { sizes } from '../../global/sizes.js';
+import { colors } from '../../global/theme.js';
 
 export const TaskFormContainer = styled.div`
   display: flex;
@@ -65,4 +66,54 @@ export const TaskFormFooter = styled.footer`
 export const MapBox = styled.aside`
   width: 100%;
   height: 40vh
+`;
+
+export const PriorityInputContainer = styled.div`
+  display: flex;
+  gap: .8rem;
+  align-items: center;
+`;
+
+export const PriorityInputLabel = styled.span`
+  font-size: 1rem;
+  line-height: 1.6rem;
+  font-weight: 700;
+  color: var(--color-dark);
+`;
+
+export const PriorityInputGroup = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: .4rem;
+  flex: 1;
+`;
+
+export const PriorityInput = styled.button`
+  height: 3.2rem;
+  border: 0;
+  border-radius: .4rem;
+  font-size: 1.2rem;
+  line-height: 2.4rem;
+  font-weight: 500;
+  cursor: pointer;
+  transition: .3s;
+
+  &:hover {
+    opacity: .8;
+  }
+`;
+
+export const PriorityInputLow = styled(PriorityInput)`
+  background-color: ${props => props.selected ? colors.p1 : 'transparent'};
+  color: ${props => props.selected ? colors.lighter : colors.p1};
+`;
+
+export const PriorityInputMedium = styled(PriorityInput)`
+  background-color: ${props => props.selected ? colors.p2 : 'transparent'};
+  color: ${props => props.selected ? colors.darker : colors.p2};
+`;
+
+export const PriorityInputHigh = styled(PriorityInput)`
+  background-color: ${props => props.selected ? colors.p3 : 'transparent'};
+  color: ${props => props.selected ? colors.lighter : colors.p3};
 `;
